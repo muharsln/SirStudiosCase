fdc5c9377b326fa26713e70ebc405d1a291b499d
muharsln <muhammedg3@gmail.com>
2022-06-09 15:02:01 +0300

    *Update: UIManager script

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -15,12 +15,10 @@ namespace SirStudiosCase.UI
 
         #region Private Serialize Field
         [SerializeField] private GameObject[] _uiPanels;
-        [SerializeField] private Button[] _panelButtons;
         #endregion
 
         #region Const Field
-        private const byte GAME_START = 0, GAME_FINISH = 1;
-        private const byte PLAY_BUTTON = 0, RESTART_BUTTON = 1;
+        private const byte GameStart = 0, GameFinish = 1;
         #endregion
 
         private void Awake() => Instance = this;
@@ -32,14 +30,14 @@ namespace SirStudiosCase.UI
             switch (_gameManager.state)
             {
                 case GameManager.GameState.Start:
-                    _uiPanels[GAME_START].SetActive(true);
+                    _uiPanels[GameStart].SetActive(true);
                     break;
                 case GameManager.GameState.Finish:
-                    _uiPanels[GAME_FINISH].SetActive(true);
+                    _uiPanels[GameFinish].SetActive(true);
                     break;
                 default:
-                    _uiPanels[GAME_START].SetActive(false);
-                    _uiPanels[GAME_FINISH].SetActive(false);
+                    _uiPanels[GameStart].SetActive(false);
+                    _uiPanels[GameFinish].SetActive(false);
                     break;
             }
         }
